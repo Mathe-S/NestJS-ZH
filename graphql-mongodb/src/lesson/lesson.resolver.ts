5f0dc7ab0495e5448b1b401978efa6a34fd32a7a
Mathe Sharvadze <sharvadzemat@gmail.com>
2020-10-03 02:31:03 +0400

    inital commit

---
+++ graphql-mongodb/src/lesson/lesson.resolver.ts
@@ -0,0 +1,21 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver(of => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
+  @Query(returns => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLessonById(id);
+  }
+
+  @Mutation(returns => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+}
